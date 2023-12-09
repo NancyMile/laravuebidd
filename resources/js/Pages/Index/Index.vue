bee42bacc0c34a0ee1e77cab23f5d8c7c9afc6c5
NancyMile <smithnancy@outlook.com>
2023-12-09 07:54:12 +0000

    persistent layout applied

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,17 +1,21 @@
 <script setup>
     import { Link } from '@inertiajs/vue3';
-    import MainLayout from '../Layouts/MainLayout.vue';
+
     defineProps({
         message: String
         })
 </script>
 
 <template>
-    <MainLayout>
         <Link href="/show">
             Show
         </Link>
         {{ message }}
-    </MainLayout>
 </template>
 
+<script>
+    import MainLayout from '../Layouts/MainLayout.vue';
+    export default {
+        layout: MainLayout
+    }
+</script>
