4843d7263497cc8f0762dec5fe3403e3f5607639
NancyMile <smithnancy@outlook.com>
2023-12-09 05:02:47 +0000

    main layout component created

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,14 +1,17 @@
 <script setup>
     import { Link } from '@inertiajs/vue3';
+    import MainLayout from '../Layouts/MainLayout.vue';
     defineProps({
         message: String
         })
 </script>
 
 <template>
-    <Link href="/show">
-        Show
-    </Link>
-    {{ message }}
+    <MainLayout>
+        <Link href="/show">
+            Show
+        </Link>
+        {{ message }}
+    </MainLayout>
 </template>
 
