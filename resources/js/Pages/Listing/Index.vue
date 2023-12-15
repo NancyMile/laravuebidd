cf19cc74a98b8c8f1e411065da1ffad0451218da
NancyMile <smithnancy@outlook.com>
2023-12-15 09:11:34 +0000

    pages  listing, Index ,Show now displaying info

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+    defineProps({
+        listings: Array,
+    })
+</script>
+
+<template>
+    <div v-for=" listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            {{ listing.street }}  {{ listing.street_number }}, {{ listing.city }} for {{ listing.price }}
+        </Link>
+    </div>
+</template>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,11 @@
+<script setup>
+ defineProps({
+    listing: Object,
+ })
+</script>
+
+<template>
+    <div>
+        {{ listing.street }}  {{ listing.street_number }}, {{ listing.city }} for {{ listing.price }}
+    </div>
+</template>
