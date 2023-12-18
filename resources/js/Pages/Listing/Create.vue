bfa37575d87588f6f00a07467c47d02a182893bc
NancyMile <smithnancy@outlook.com>
2023-12-18 09:06:41 +0000

    validating the create listing

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -21,34 +21,58 @@ const create = () => form.post('/listing')
             <div>
                 <label>Beds</label>
                 <input type="text" v-model="form.beds" />
+                <div v-if="form.errors.beds">
+                    {{ form.errors.beds }}
+                </div>
             </div>
         <div>
             <label>Baths</label>
                 <input type="text" v-model="form.baths"/>
+                <div v-if="form.errors.baths">
+                    {{ form.errors.baths }}
+                </div>
         </div>
         <div>
             <label>Area</label>
                 <input type="text" v-model="form.area" />
+                <div v-if="form.errors.area">
+                    {{ form.errors.area }}
+                </div>
         </div>
         <div>
             <label>City</label>
                 <input type="text" v-model="form.city" />
+                <div v-if="form.errors.city">
+                    {{ form.errors.city }}
+                </div>
         </div>
         <div>
             <label>Post Code</label>
                 <input type="text" v-model="form.code" />
+                <div v-if="form.errors.code">
+                    {{ form.errors.code }}
+                </div>
         </div>
         <div>
             <label>Street</label>
                 <input type="text" v-model="form.street"/>
+                <div v-if="form.errors.street">
+                    {{ form.errors.street }}
+                </div>
         </div>
         <div>
             <label>Street Number</label>
                 <input type="text" v-model="form.street_number"/>
+                <div v-if="form.errors.street_number">
+                    {{ form.errors.street_number }}
+                </div>
         </div>
         <div>
             <label>Price</label>
                 <input type="text" v-model="form.price" />
+                <div v-if="form.errors.price">
+                    {{ form.errors.price }}
+                </div>
         </div>
         <div>
             <button type="submit">Create</button>
