753d401cc7e02dd203d9788b8d6e136a22825b79
NancyMile <smithnancy@outlook.com>
2023-12-19 04:27:48 +0000

    main layout tailwindcss styles applied

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -6,24 +6,29 @@ import { computed } from 'vue';
  const flashSuccess =  computed(
     () => page.props.flash.success,
  )
-//const timer = ref(0);
-
-//setInterval(() => timer.value++, 1000)
 </script>
 
 <template>
-    <Link :href="route('listing.index')">Listings</Link>
-    <Link :href="route('listing.create')">New Listing</Link>
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
-    <slot>Default</slot>
+    <header class="w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listing</Link>
+                </div>
+                <div class="text-xl text-indigo-600">
+                    <Link :href="route('listing.index')">Laravue</Link>
+                </div>
+                <div>
+                    <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white p-2 cursor-pointer font-medium rounded-md">+ New Listing</Link>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 bg-green-300 text-center text-white font-bold p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
+    </main>
 </template>
-
-<style scoped>
-    .success {
-        background-color: green;
-        color: white;
-    }
-
-</style>
