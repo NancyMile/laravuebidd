e8706e784d0ac98f22efae91a58474e9c307ccca
NancyMile <smithnancy@outlook.com>
2023-12-19 09:30:40 +0000

    directive layout component applied to edit page

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -21,65 +21,65 @@ const update = () => form.put(route('listing.update',{listing: props.listing.id 
 
 <template>
     <form @submit.prevent="update">
-        <div>
-            <div>
-                <label>Beds</label>
-                <input type="text" v-model="form.beds" />
+        <div class="grid grid-cols-6 gap-4">
+            <div class="col-span-2">
+                <label class="label">Beds</label>
+                <input type="text" v-model="form.beds"  class="input"/>
                 <div v-if="form.errors.beds">
                     {{ form.errors.beds }}
                 </div>
             </div>
-        <div>
-            <label>Baths</label>
-                <input type="text" v-model="form.baths"/>
+        <div class="col-span-2">
+            <label class="label">Baths</label>
+                <input type="text" v-model="form.baths" class="input"/>
                 <div v-if="form.errors.baths">
                     {{ form.errors.baths }}
                 </div>
         </div>
-        <div>
-            <label>Area</label>
-                <input type="text" v-model="form.area" />
+        <div class="col-span-2">
+            <label class="label">Area</label>
+                <input type="text" v-model="form.area"  class="input"/>
                 <div v-if="form.errors.area">
                     {{ form.errors.area }}
                 </div>
         </div>
-        <div>
-            <label>City</label>
-                <input type="text" v-model="form.city" />
+        <div class="col-span-4">
+            <label class="label">City</label>
+                <input type="text" v-model="form.city"  class="input"/>
                 <div v-if="form.errors.city">
                     {{ form.errors.city }}
                 </div>
         </div>
-        <div>
-            <label>Post Code</label>
-                <input type="text" v-model="form.code" />
+        <div class="col-span-2">
+            <label class="label">Post Code</label>
+                <input type="text" v-model="form.code"  class="input"/>
                 <div v-if="form.errors.code">
                     {{ form.errors.code }}
                 </div>
         </div>
-        <div>
-            <label>Street</label>
-                <input type="text" v-model="form.street"/>
+        <div class="col-span-4">
+            <label class="label">Street</label>
+                <input type="text" v-model="form.street" class="input"/>
                 <div v-if="form.errors.street">
                     {{ form.errors.street }}
                 </div>
         </div>
-        <div>
-            <label>Street Number</label>
-                <input type="text" v-model="form.street_number"/>
+        <div class="col-span-2">
+            <label class="label">Street Number</label>
+                <input type="text" v-model="form.street_number" class="input"/>
                 <div v-if="form.errors.street_number">
                     {{ form.errors.street_number }}
                 </div>
         </div>
-        <div>
-            <label>Price</label>
-                <input type="text" v-model="form.price" />
+        <div class="col-span-6">
+            <label class="label">Price</label>
+                <input type="text" v-model="form.price"  class="input"/>
                 <div v-if="form.errors.price">
                     {{ form.errors.price }}
                 </div>
         </div>
         <div>
-            <button type="submit">Edit</button>
+            <button type="submit" class="primary-btn">Edit</button>
         </div>
         </div>
     </form>
