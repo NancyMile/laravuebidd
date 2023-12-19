4b27003cec56541327292c06a15e05777da2c7d0
NancyMile <smithnancy@outlook.com>
2023-12-19 07:24:59 +0000

    page show displaying listing info, styles applied

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,5 +1,8 @@
 <script setup>
 import ListingAddress from '@/Components/ListingAddress.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue';
 
 defineProps({
     listing: Object,
@@ -7,7 +10,21 @@ defineProps({
 </script>
 
 <template>
-    <div>
-        <ListingAddress  :listing="listing"/>
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="md:col-span-7 flex items-center w-full font-medium text-gray-500">
+            <div class="w-full text-center">
+                No Images
+            </div>
+        </Box>
+        <div class=" md:col-span-5 flex flex-col gap-3">
+            <Box>
+                <Price :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray" />
+            </Box>
+            <Box>
+                Making an offer
+            </Box>
+        </div>
     </div>
 </template>
