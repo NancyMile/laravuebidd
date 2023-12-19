611ece83d28cb2661bfae11d9d190335350337db
NancyMile <smithnancy@outlook.com>
2023-12-19 05:07:04 +0000

    Component UI Box, created and applied to index vue

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="border border-gray-200 rounded-md p-4 shadow-sm text-gray-400">
+        <slot/>
+    </div>
+</template>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
 import ListingAddress from '@/Components/ListingAddress.vue';
+import Box from '@/Components/UI/Box.vue'
     defineProps({
         listings: Array,
     })
 </script>
 
 <template>
-    <div v-for=" listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show',{listing: listing.id})">
-                <ListingAddress :listing="listing" />
-            </Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit',{listing: listing.id})">Edit</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy',{listing: listing.id})" method="delete">Delete</Link>
-        </div>
+    <div class="grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 gap-2">
+        <Box v-for=" listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show',{listing: listing.id})">
+                    <ListingAddress :listing="listing" />
+                </Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit',{listing: listing.id})">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy',{listing: listing.id})" method="delete">Delete</Link>
+            </div>
+        </Box>
     </div>
 </template>
