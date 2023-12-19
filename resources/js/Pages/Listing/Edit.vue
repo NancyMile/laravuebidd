fff57cda058e2d22a4af9f9d229a6d535b3ce26b
NancyMile <smithnancy@outlook.com>
2023-12-19 10:15:07 +0000

    directive layout component input-error created and applied to create and edit forms

--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -25,56 +25,56 @@ const update = () => form.put(route('listing.update',{listing: props.listing.id 
             <div class="col-span-2">
                 <label class="label">Beds</label>
                 <input type="text" v-model="form.beds"  class="input"/>
-                <div v-if="form.errors.beds">
+                <div v-if="form.errors.beds" class="input-error">
                     {{ form.errors.beds }}
                 </div>
             </div>
         <div class="col-span-2">
             <label class="label">Baths</label>
                 <input type="text" v-model="form.baths" class="input"/>
-                <div v-if="form.errors.baths">
+                <div v-if="form.errors.baths" class="input-error">
                     {{ form.errors.baths }}
                 </div>
         </div>
         <div class="col-span-2">
             <label class="label">Area</label>
                 <input type="text" v-model="form.area"  class="input"/>
-                <div v-if="form.errors.area">
+                <div v-if="form.errors.area" class="input-error">
                     {{ form.errors.area }}
                 </div>
         </div>
         <div class="col-span-4">
             <label class="label">City</label>
                 <input type="text" v-model="form.city"  class="input"/>
-                <div v-if="form.errors.city">
+                <div v-if="form.errors.city" class="input-error">
                     {{ form.errors.city }}
                 </div>
         </div>
         <div class="col-span-2">
             <label class="label">Post Code</label>
                 <input type="text" v-model="form.code"  class="input"/>
-                <div v-if="form.errors.code">
+                <div v-if="form.errors.code" class="input-error">
                     {{ form.errors.code }}
                 </div>
         </div>
         <div class="col-span-4">
             <label class="label">Street</label>
                 <input type="text" v-model="form.street" class="input"/>
-                <div v-if="form.errors.street">
+                <div v-if="form.errors.street" class="input-error">
                     {{ form.errors.street }}
                 </div>
         </div>
         <div class="col-span-2">
             <label class="label">Street Number</label>
                 <input type="text" v-model="form.street_number" class="input"/>
-                <div v-if="form.errors.street_number">
+                <div v-if="form.errors.street_number" class="input-error">
                     {{ form.errors.street_number }}
                 </div>
         </div>
         <div class="col-span-6">
             <label class="label">Price</label>
                 <input type="text" v-model="form.price"  class="input"/>
-                <div v-if="form.errors.price">
+                <div v-if="form.errors.price" class="input-error">
                     {{ form.errors.price }}
                 </div>
         </div>
