f09e06fce3aaaded8ab575ac8b70720598584354
NancyMile <smithnancy@outlook.com>
2023-12-22 02:32:27 +0000

    LintingPolicy created, implemeted on the resouce controller Listing controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use Auth;
 use Illuminate\Http\Request;
+use PhpParser\Builder\Function_;
 
 class ListingController extends Controller
 {
+
+    public  function __construct()
+    {
+        $this->authorizeResource(Listing::class,'listing');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -20,6 +28,7 @@ class ListingController extends Controller
      */
     public function create()
     {
+     //   $this->authorize('create',Listing::class);
         return inertia('Listing/Create');
     }
 
@@ -48,6 +57,10 @@ class ListingController extends Controller
      */
     public function show(Listing $listing)
     {
+        // if(Auth::user()->cannot('view',$listing)){
+        //     abort(403);
+        // }
+        //$this->authorize('view',$listing);
         return inertia('Listing/Show',['listing' => $listing]);
     }
 
