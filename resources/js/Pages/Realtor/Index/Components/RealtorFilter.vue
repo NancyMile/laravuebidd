d0a616c3703ab580b82e7b2c91d9f87ef9da048a
NancyMile <smithnancy@outlook.com>
2023-12-29 02:01:10 +0000

    realtorFilter component using watchers

--- resources/js/Pages/Realtor/Index/Components/RealtorFilter.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilter.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { reactive, watch } from 'vue'
+import {Inertia} from '@inertiajs/inertia'
+
+const filterForm = reactive({
+    deleted: false,
+});
+
+watch(
+        filterForm, () => Inertia.get(
+            route('realtor.listing.index'),
+            filterForm,
+            {
+                preserveState: true,
+                preserveScroll: true,
+            },
+        ),
+);
+
 </script>
 
 <template>
 <form>
     <div class="mb-4 mt-4 flex flex-wrap gap-2">
         <div class="flex flex-nowrap items-center gap-2">
-            <input  type="checkbox" id="deleted" class="w-4 h-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+            <input  type="checkbox"  v-model="filterForm.deleted" id="deleted" class="w-4 h-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
             <label for="deleted">Deleted</label>
         </div>
     </div>
