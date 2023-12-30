34e76bb6098afc6d4bda31c0a6ea7410f5cb70f1
NancyMile <smithnancy@outlook.com>
2023-12-30 09:38:55 +0000

    realtor listing images method storage implemeted, realtor listingImage, create form edited to receive multi images

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\ListingImage;
 use Illuminate\Http\Request;
 
 class RealtorListingImageController extends Controller
@@ -18,6 +19,19 @@ class RealtorListingImageController extends Controller
    // This action will accept the data sent through  the form
     public function store(Listing $listing, Request $request)
     {
+        //check if there is a file uploaded
+        if($request->hasFile('images')){
+            foreach($request->file('images') as $file){
+                //saves the image on images folder of public
+                $path = $file->store('images', 'public');
 
+                //save on db based on the relationship images
+                $listing->images()->save(new ListingImage([
+                    'filename' => $path
+                ]));
+            }
+        }
+
+        return redirect()->back()->with('success','Images uploaded');
     }
 }
--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -5,8 +5,8 @@ defineProps({
 </script>
 
 <template>
- <form :action="route('realtor.listing.image.store', {listing: listing.id})" method="post" >
-    <input type="file" name="files" multiple>
+ <form :action="route('realtor.listing.image.store', {listing: listing.id})" method="post" enctype="multipart/form-data">
+    <input type="file" name="images[]" multiple>
     <button type="submit" class="btn-outline">Upload</button>
  </form>
 </template>
