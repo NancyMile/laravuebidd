12c6bb5854062d3d1489209408a410d641964dcb
NancyMile <smithnancy@outlook.com>
2023-12-30 11:32:20 +0000

    styles applied to the upload input,realtor listing image create page

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Box from '@/Components/UI/Box.vue'
 import { useForm } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const canUpload = computed(() => form.images.length)
 
 const props = defineProps({
     listing: Object
@@ -32,9 +35,12 @@ const reset = () => form.reset('images')
     <Box>
         <template #header>Upload Images</template>
         <form @submit.prevent="upload">
-            <input type="file" multiple @input="addFiles">
-            <button type="submit" class="btn-outline">Upload</button>
-            <button type="reset" class="btn-outline" @click="reset">Reset</button>
+            <section class="flex items-center gap-2 my-2">
+                <input type="file" class="border rounded-md file:px-4 file:py-2 border-gray-200 file:text-gray-700 file:border-0 file:bg-gray-100 file:font-medium
+                    file:hover:bg-gray-200 file:hover:cursor-pointer file:mr-4" multiple @input="addFiles">
+                <button type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed" :disabled="!canUpload">Upload</button>
+                <button type="reset" class="btn-outline" @click="reset">Reset</button>
+            </section>
         </form>
     </Box>
 </template>
