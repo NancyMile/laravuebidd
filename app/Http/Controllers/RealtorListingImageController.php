56025939b4cee7105cd7f6fabca67c32137eaf0d
NancyMile <smithnancy@outlook.com>
2023-12-30 07:38:53 +0000

    RealtorListingImageController created with  methods create and store, route added on web

---
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RealtorListingImageController extends Controller
+{
+    //this action will render the form
+    public  function create()
+    {
+
+    }
+
+   // This action will accept the data sent through  the form
+    public function store()
+    {
+
+    }
+}
