b61702e9a69d4f55a11713c84aef598a5c239fa1
NancyMile <smithnancy@outlook.com>
2024-01-03 10:30:04 +0000

    realtor view offers by listing

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -4,7 +4,8 @@
     import Price from '@/Components/Price.vue';
     import Box from '@/Components/UI/Box.vue';
     import { Link } from '@inertiajs/vue3';
-import { computed } from 'vue';
+    import { computed } from 'vue';
+    import Offer from '@/Pages/Realtor/Show/Components/Offer.vue'
 
     const props = defineProps({
         listing: Object,
@@ -21,8 +22,8 @@ import { computed } from 'vue';
         <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
             <div class="w-full font-medium text-center text-gray-500">No offers</div>
         </Box>
-        <div v-else class="flex md:col-span-7 items-center">
-            Offers!
+        <div v-else class="flex md:col-span-7 items-center gap-2">
+            <Offer v-for="offer in listing.offers" :key="offer.id" :offer="offer" :listing-price="listing.price" class="mb-4"/>
         </div>
         <Box class="md:col-span-5">
             <template #header>Basic Info</template>
---
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import Price from '@/Components/Price.vue';
+    import { computed } from 'vue';
+    import Box from '@/Components/UI/Box.vue'
+    import { Link } from '@inertiajs/vue3';
+
+
+    const props = defineProps({
+        offer: Object,
+        listingPrice: Number
+    })
+
+    const difference = computed(() => props.offer.amount - props.listingPrice)
+
+    const madeOn = computed(() => new Date(props.offer.created_at).toDateString())
+</script>
+<template>
+    <Box>
+        <template #header>Offer {{ offer.id }}</template>
+        <section class="flex  items-center justify-between">
+            <div>
+                <Price :price="offer.amount" class="text-xl"/>
+                <div class="text-gray-500">
+                    Difference <Price :price="difference"/>
+                </div>
+                <div class="text-gray-500 text-sm">
+                    made by ()
+                </div>
+                <div class="text-gray-500 text-sm">
+                    Mede on {{ madeOn }}
+                </div>
+            </div>
+            <Link class="btn-outline text-xs font-medium" as="button">Accept</Link>
+        </section>
+    </Box>
+</template>
