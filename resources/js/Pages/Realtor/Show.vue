86f49acec359ba258289f16884c0400dfc3ce7da
NancyMile <smithnancy@outlook.com>
2024-01-03 09:10:51 +0000

    realtor display offer page

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,5 +1,34 @@
 <script setup>
+    import ListingAddress from '@/Components/ListingAddress.vue';
+    import ListingSpace from '@/Components/ListingSpace.vue';
+    import Price from '@/Components/Price.vue';
+    import Box from '@/Components/UI/Box.vue';
+    import { Link } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+    const props = defineProps({
+        listing: Object,
+    })
+
+    const hasOffers = computed(() => props.listing.offers.length)
+
 </script>
 <template>
-test
+    <div class="mb-3">
+        <Link :href="route('realtor.listing.index')"> ⬅️ Back to listings</Link>
+    </div>
+    <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+            <div class="w-full font-medium text-center text-gray-500">No offers</div>
+        </Box>
+        <div v-else class="flex md:col-span-7 items-center">
+            Offers!
+        </div>
+        <Box class="md:col-span-5">
+            <template #header>Basic Info</template>
+            <Price :price="listing.price" class="text-2xl font-bold"/>
+            <ListingSpace :listing="listing" class="text-lg"/>
+            <ListingAddress :listing="listing" class="text-gray-500"/>
+        </Box>
+    </section>
 </template>
